0bebd3e758f5269eba94d81f32e4b1f46201fcd2
Mukesh Anand <Mukeshanandmax@gmail.com>
2023-09-09 12:32:26 +0530

    User entity and Repository added

---
+++ E-wallet/src/main/java/com/practice/Ewallet/User/User.java
@@ -0,0 +1,20 @@
+package com.practice.Ewallet.User;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "internalId")
+    private Long id;
+    private String userId;
+    private String email;
+
+}
---
+++ E-wallet/src/main/java/com/practice/Ewallet/User/UserRequest.java
@@ -0,0 +1,14 @@
+package com.practice.Ewallet.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserRequest {
+    private String userId;
+    private String email;
+}
